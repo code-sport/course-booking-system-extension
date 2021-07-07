8c885b119d0315b152a277bdba076fc66feec6d0
code.sport <git@codesport.info>
2021-07-07 22:07:31 +0200

    Fix of review error: Data Must be Sanitized, Escaped, and Validated
    When you include POST/GET/REQUEST/FILE calls in your plugin, it's important to sanitize, validate, and escape them. The goal here is to prevent a user from accidentally sending trash data through the system, as well as protecting them from potential security issues.
    
    SANITIZE: Data that is input (either by a user or automatically) must be sanitized as soon as possible. This lessens the possibility of XSS vulnerabilities and MITM attacks where posted data is subverted.
    
    VALIDATE: All data should be validated, no matter what. Even when you sanitize, remember that you don’t want someone putting in ‘dog’ when the only valid values are numbers.
    
    ESCAPE: Data that is output must be escaped properly when it is echo'd, so it can't hijack admin screens. There are many esc_*() functions you can use to make sure you don't show people the wrong data.
    
    To help you with this, WordPress comes with a number of sanitization and escaping functions. You can read about those here:
    •	https://developer.wordpress.org/plugins/security/securing-input/
    •	https://developer.wordpress.org/plugins/security/securing-output/
    
    Remember: You must use the most appropriate functions for the context. If you’re sanitizing email, use sanitize_email(), if you’re outputting HTML, use esc_html(), and so on.
    
    An easy mantra here is this:
    
    Sanitize early
    Escape Late
    Always Validate
    
    Clean everything, check everything, escape everything, and never trust the users to always have input sane data. After all, users come from all walks of life.

--- includes/ajax.php
+++ includes/ajax.php
@@ -31,8 +31,8 @@ add_action('wp_ajax_nopriv_cbse_participants_via_mail', 'cbse_participants_via_m
 function cbse_participants_via_mail()
 {
     check_ajax_referer('cbse_event_head_courses');
-    $courseId = intval($_POST['course_id']);
-    $date = $_POST['date'];
+    $courseId = intval(sanitize_key($_POST['course_id']));
+    $date = sanitize_key($_POST['date']);
     $sent = cbse_sent_mail_with_course_date_bookings($courseId, $date, get_current_user_id());
     $args = array(
         'course_id' => $courseId,
