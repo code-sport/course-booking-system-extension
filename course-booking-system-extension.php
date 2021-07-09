845df61d4d63ca72b3558059a5482c1d21a5d9b9
code.sport <git@codesport.info>
2021-07-09 22:19:07 +0200

    Set version to 0.0.7

--- course-booking-system-extension.php
+++ course-booking-system-extension.php
@@ -3,7 +3,7 @@
  * Plugin Name: Course Booking System Extension
  * Plugin URI: https://github.com/code-sport/course-booking-system-extension
  * Description: Extension for Course Booking System
- * Version: 0.0.6
+ * Version: 0.0.7
  * Author: Code.Sport
  * Author URI: https://github.com/code-sport/
  * Text Domain: cbse
