ddc1d89dacd431c4ba9fee2c36928d1ab49c3941
code.sport <git@codesport.info>
2021-07-18 22:28:59 +0200

    Remove warning that mkdir already exists.

--- includes/templates.php
+++ includes/templates.php
@@ -4,19 +4,21 @@ class CBSE_Templates
 {
     public function init()
     {
-        do_action( 'qm/info', 'init' );
+        do_action('qm/info', 'init');
         add_action('wp_head', [$this, 'cbse_required_files']);
     }
 
     function cbse_required_files()
     {
-        do_action( 'qm/info', 'cbse_required_files' );
+        do_action('qm/info', 'cbse_required_files');
         // Shortcodes / Table Header
         $file_theme = get_template_directory() . '/mp-timetable/shortcodes/cbse_event_head_courses-single.php';
         $file_plugin = plugin_dir_path(__FILE__) . '../templates/shortcodes/cbse_event_head_courses-single.php';
 
         if (!file_exists($file_theme)) {
-            mkdir(dirname($file_theme), 0777, true); // trick to make sure the folder exists first
+            if (!file_exists(dirname($file_theme))) {
+                mkdir(dirname($file_theme), 0777, true); // trick to make sure the folder exists first
+            }
             if (!copy($file_plugin, $file_theme)) {
                 add_action('admin_notices', [$this, 'cbs_required_file_event_head_courses_single']);
             }
